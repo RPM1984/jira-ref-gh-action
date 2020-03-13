03f2312aaa75e1883be7a76737007af22e7f5e84
RPM1984 <rpmir1@gmail.com>
2020-03-13 13:58:01 +1100

    Fixed field

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const projectName = core.getInput('project-name');
-const prDescription =
-    github.context.payload &&
-    github.context.payload.pull_request &&
-    github.context.payload.pull_request.description;
-const isValid = prDescription.includes(projectName);
+const prDescription = github.context.payload &&
+                      github.context.payload.pull_request &&
+                      github.context.payload.pull_request.body;
+const isValid = prDescription &&
+                prDescription.includes(projectName);
 core.info(prDescription);
 if (!isValid) {
     core.setFailed(`PR description doesn't include ${projectName}.`);
